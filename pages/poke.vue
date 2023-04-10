cd4455aaa1c10f8d304a0cebf388af79f2247c73
daniela <slazardaniela05@gmail.com>
2023-04-10 13:36:55 -0500

    primer commit

---
+++ pages/poke.vue
@@ -0,0 +1,93 @@
+<template>
+
+    <div class="principal">
+        <div class="vacio"></div>
+
+        
+        <h1><b> PokeDEx</b> </h1>
+        
+
+        <div class="navegacion">
+            <input type="text" placeholder="Buscar pokemon">
+        </div>
+
+        <div class="tarjeta">
+            <div class="imag"><img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png" alt=""></div>
+            <button>
+                <p> <b>Squirtle</b> </p>
+            </button>
+            <!-- <p> <b>Squirtle</b> </p> -->
+            
+        </div>
+
+    </div>
+ 
+
+</template>
+
+<style>
+*{
+    padding: 0%;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+.principal{
+    height: 100vh;
+    width: 100%;
+    background-color: aquamarine;
+   
+
+}
+.vacio{
+    height: 23px;
+}  
+
+h1{
+    width: 97%;
+    height: 85px;
+    margin: auto; 
+    background-color: white;
+    font-size: 75px;
+    text-align: center;
+
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.navegacion{
+    margin-top: 20px;
+    margin-left: 28px;
+}
+input{
+    height: 38px;
+    width: 280px;
+    border: solid 2px rgba(128, 128, 128, 0.541);
+    border-radius: 5px;
+
+}
+
+.tarjeta{
+    height: 330px;
+    width: 280px;
+    margin-left: 28px;
+    margin-top: 20px;
+    border: solid 5px white;
+    border-radius: 5px;
+    background-color: red;
+    box-shadow: 4px 5px 4px 5px rgba(0, 0, 0, 0.466);
+}
+.tarjeta p{
+    color: white;
+    text-align: center;
+    font-size: 25px;
+}
+button{
+    height: 40px;
+    width: 100px;
+    margin-left: 80px;
+}
+</style>
+
+
---
+++ pages/prueba.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <p> {{ nombre }} </p>
+        <img v-if="mostrar" src="https://i.pinimg.com/736x/b0/e9/11/b0e91175f0772af0f80ab3a544587443.jpg" alt="" width="500px">
+        <button @click="cambiar" >Cambiar nombre</button>
+
+    </div>
+
+</template>
+
+<script>
+export default{
+    name: 'PruebaPage',
+    data() {
+        return {
+            nombre:'Lizeth Daniela',
+            mostrar: true
+        }
+    },
+
+    methods: {
+        cambiar (){
+            if(this.nombre == 'Lizeth Daniela'){
+                this.nombre = 'Salazar Anaya'
+                this.mostrar= false
+            }else{
+                this.nombre= 'Lizeth Daniela'
+                this.mostrar=true
+            }
+            
+        }
+    }
+
+}
+</script>
+
+<style>
+button{
+  
+    background-color: rgba(238, 130, 238, 0.384) ;
+    border: solid 1px red;
+}
+button:hover{
+    background-color: rgba(137, 43, 226, 0.281);
+}
+</style>
